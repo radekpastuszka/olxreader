4148972928658abe6e69b16c9b7b9648e256c0e4
Radoslaw Pastuszka <Radoslaw.Pastuszka@unit4.com>
2020-04-30 17:59:25 +0200

    added csv support

---
+++ WorkerService1/Models/CsvRow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WorkerService1.Models
+{
+    class CsvRow
+    {
+        public DateTime Date { get; set; }
+        public int WroclawToRent { get; set; }
+        public int WroclawToSell { get; set; }
+        public int GdanskToRent { get; set; }
+        public int GdanskToSell { get; set; }
+        public int WarszawaToRent { get; set; }
+        public int WarszawaToSell { get; set; }
+        public int KrakowToRent { get; set; }
+        public int KrakowToSell { get; set; }
+        public int PoznanToRent { get; set; }
+        public int PoznanToSell { get; set; }
+        public int LodzToRent { get; set; }
+        public int LodzToSell { get; set; }
+        public int TotalToRent { get; set; }
+        public int TotalToSell { get; set; }
+    }
+}
--- WorkerService1/Worker.cs
+++ WorkerService1/Worker.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
+using CsvHelper;
 using HtmlAgilityPack;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WorkerService1.Models;
 
 namespace WorkerService1
 {
@@ -21,17 +25,23 @@ namespace WorkerService1
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-
             Dictionary<string, string> cities = new Dictionary<string, string>();
             cities.Add("Total", "");
             cities.Add("Wrocław", "wroclaw");
             cities.Add("Gdańsk", "gdansk");
+            cities.Add("Warszawa", "warszawa");
+            cities.Add("Kraków", "krakow");
+            cities.Add("Poznań", "poznan");
+            cities.Add("Lodz", "lodz");
 
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
 
-                foreach(var city in cities)
+                List<CsvRow> records = new List<CsvRow>();
+                CsvRow newRow = new CsvRow();
+
+                foreach (var city in cities)
                 {
                     try
                     {
@@ -57,15 +67,74 @@ namespace WorkerService1
                         .SelectSingleNode($"//a[@href=\"https://www.olx.pl/nieruchomosci/mieszkania/sprzedaz/{serachString}\"]/following-sibling::span")?.InnerText;
 
                         int sellCount = Convert.ToInt32(sellString.Replace(" ", ""));
+
+                        newRow.Date = DateTime.Now;
+
+                        switch(city.Key)
+                        {
+                            case "Total":
+                                newRow.TotalToRent = rentCount;
+                                newRow.TotalToSell = sellCount;
+                                break;
+                            case "Wrocław":
+                                newRow.WroclawToRent = rentCount;
+                                newRow.WroclawToSell = sellCount;
+                                break;
+                            case "Gdańsk":
+                                newRow.GdanskToRent = rentCount;
+                                newRow.GdanskToSell = sellCount;
+                                break;
+                            case "Warszawa":
+                                newRow.WarszawaToRent = rentCount;
+                                newRow.WarszawaToSell = sellCount;
+                                break;
+                            case "Kraków":
+                                newRow.KrakowToRent = rentCount;
+                                newRow.KrakowToSell = sellCount;
+                                break;
+                            case "Poznań":
+                                newRow.PoznanToRent = rentCount;
+                                newRow.PoznanToSell = sellCount;
+                                break;
+                            case "Lodz":
+                                newRow.LodzToRent = rentCount;
+                                newRow.LodzToSell = sellCount;
+                                break;
+
+                            default:
+                                break;
+
+                        }
+                       
                     }
                     catch(Exception ex)
                     {
                         _logger.LogInformation(ex.Message, DateTimeOffset.Now);
                     }
                 }
-                
 
-                await Task.Delay(1000, stoppingToken);
+
+                string path = new FileInfo(AppDomain.CurrentDomain.BaseDirectory).Directory.Parent.Parent.Parent.FullName;
+                string olxRentCsv = $"{path}/olxData.csv";
+
+                if (File.Exists(olxRentCsv))
+                {
+                    using (var reader = new StreamReader(olxRentCsv))
+                    using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                    {
+                        records = csv.GetRecords<CsvRow>().ToList();
+                    }
+                }
+
+                records.Add(newRow);
+
+                using (var writer = new StreamWriter(olxRentCsv))
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csv.WriteRecords(records);
+                }
+                
+                await Task.Delay(1000*60*60, stoppingToken);
             }
         }
     }
